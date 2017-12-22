26d2fc28056663fb89a7a93fd49387546a3c34d7
Ariana <ariana.aqifi@gmail.com>
2017-12-22 09:19:43 +0100

    All

---
+++ js/scroll.js
@@ -0,0 +1,34 @@
+jQuery.fn.topLink = function () {
+    return this.each(function () {
+        var elem  = $(this)
+            speed = 400;
+        elem.hide();
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= 1) {
+                elem.slideDown(speed)
+            } else {
+                elem.slideUp(speed)
+            }
+        })
+    })
+};
+(function () {
+    $(document).ready(function () {
+        $("#toTheTop").topLink();
+        $("#toTheTop").on("click", function () {
+            return $("html, body").animate({ scrollTop: 0 });
+        });
+
+        $(".navlist a").on("click", function (a) {
+            a.preventDefault();
+            var attr   = $(this).attr("href"),
+                offset = $(attr).offset().top;
+            $("html, body").animate({
+                scrollTop: offset
+            }, function () {
+                window.location.hash = attr
+            });
+            return false
+        })
+    })
+})()
